7d23f679f4f99ffc3bf2462a611035ca779f19d1
Mohammad <raisimohammad@gmail.com>
2025-03-08 19:22:40 +0330

    csrf token added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 from blueprints.general import app as general
 from blueprints.user import app as user
 from blueprints.admin import app as admin 
@@ -14,6 +15,8 @@ app.register_blueprint(admin)
 app.config["SQLALCHEMY_DATABASE_URI"]=config.SQLALCHEMY_DATABASE_URI
 app.config["SECRET_KEY"]=config.SECRET_KEY
 extenoins.db.init_app(app)
+
+csrf=CSRFProtect(app)
 
 with app.app_context():
     extenoins.db.create_all()
